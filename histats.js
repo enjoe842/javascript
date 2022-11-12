64066558bdfa373610be8c33c0f05043d1d55b07
Enjoe <33560815+enjoe842@users.noreply.github.com>
2022-11-12 22:14:30 +0700

    Update and rename hitat.js to histats.js

--- histats.js
+++ histats.js
@@ -1,9 +1,9 @@
 var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4676163,4,0,0,0,00010000']);
+_Hasync.push(['Histats.start', '1,4331xxx,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('https://cdn.jsdelivr.net/gh/enjoe842/javascript@main/hitat15.js');
+hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();
